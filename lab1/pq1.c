6300ae1f5a41a24aca4b7a3d763e9b6fbd358444
Geet <geet1998patel@gmail.com>
2019-01-21 05:17:16 +0530

    pq done. don't ever fucking toush that thing again

--- lab1/pq1.c
+++ lab1/pq1.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 int pq[INT16_MAX][4];
 int front;
@@ -12,45 +13,31 @@ void create(){
 }
 
 void insert(int n1, int n2, int n3, int p){
-    printf("Insert called for %d %d %d %d\n", n1, n2, n3, p);
+    front = 0;
     if (front == -1 && rear == -1) {
-        front = 0;
-        rear += 1;
-        printf("If : Inserting\n");
         pq[rear][0] = n1;
         pq[rear][1] = n2;
         pq[rear][2] = n3;
         pq[rear][3] = p;
+        rear += 1;
         return;
     }
     else{
         int i;
         i = rear;
         int j = 0;
-        printf("Else : \n");
-        printf("i %d\n",i);
         while (p > pq[i][3] && i >= 0) {
-            printf("In while Inserting\n");
             pq[i+1][0] = pq[i][0];
             pq[i+1][1] = pq[i][1];
             pq[i+1][2] = pq[i][2];
             pq[i+1][3] = pq[i][3];
-            printf("%d %d\n", pq[i][0], pq[i+1][0]);
-            printf("%d %d\n", pq[i][1], pq[i+1][1]);
-            printf("%d %d\n", pq[i][2], pq[i+1][2]);
-            printf("%d %d\n", pq[i][3], pq[i+1][3]);
             i -= 1;
         }
-        i+=1;
+        i += 1;
         pq[i][0] = n1;
         pq[i][1] = n2;
         pq[i][2] = n3;
         pq[i][3] = p;
-        printf("i %d\n",i);
-        printf("%d %d\n", pq[i][0], n1);
-        printf("%d %d\n", pq[i][1], n2);
-        printf("%d %d\n", pq[i][2], n3);
-        printf("%d %d\n", pq[i][3], p);
         rear += 1;
         return;
     }
@@ -72,20 +59,30 @@ void rem(){
     pq[rear][2] = 0;
     pq[rear][3] = 0;
     rear -= 1;
+
+    if(rear == -1){
+        front = -1;
+    }
+
     return;
+}
+
+bool isqempty(){
+    if(front == -1 && rear == -1){
+        return true;
+    }
+    return false;
 }
 
 void disp(){
     int i = 0;
-    if(front == -1 && rear == -1){
+    if (front == -1 && rear == -1) {
         printf("Queue is empty!!\n");
         return;
     }
     else{
         front = 0;
-        int i = 0;
-        printf("Front %d Rear %d\n", front, rear);
-        while(i <= rear){
+        while (i <= rear) {
             printf("%d %d %d %d -> ", pq[i][0], pq[i][1], pq[i][2], pq[i][3]);
             i += 1;
         }
@@ -97,9 +94,11 @@ void disp(){
 int main(){
 
     create();
+    printf("%d\n",isqempty());
     disp();
     insert(1, 2, 3, 4);
     disp();
+    printf("%d\n",isqempty());
     insert(1, 3, 5, 5);
     disp();
     insert(1, 4, 6, 7);
@@ -112,5 +111,12 @@ int main(){
     disp();
     insert(8, 7, 6, 6);
     disp();
+    rem();
+    disp();
+    rem();
+    disp();
+    rem();
+    disp();
+    printf("%d\n",isqempty());
     return 0;
 }

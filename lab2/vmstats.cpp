b54d37e2115a13edb185b5aaf54b3792157f0c68
Geet <geet1998patel@gmail.com>
2019-02-13 13:13:49 +0530

    Stats: Done

--- lab2/vmstats.cpp
+++ lab2/vmstats.cpp
@@ -1,22 +1,20 @@
-#include "vmsim.cpp"
+#include "vmsim2.cpp"
 
-int main(int argc, char** argv){
+int main(){
+    std::cout << std::fixed;
+    std::cout << std::setprecision(2);
+
+    cout << ">";
 
     char* opt = (char*)"opt";
     char* lru = (char*)"lru";
     char* fifo = (char*)"fifo";
-    //cout << opt << endl << lru << endl << fifo << endl;
-    //cout << argv[1] << endl;
-    int min_num = isdigit2(argv[1]);
-    //cout << min_num << endl;
-    //cout << argv[2] << endl;
-    int max_num = isdigit2(argv[2]);
-    //cout << max_num << endl;
-    //cout << argv[3] << endl;
-    int leap = isdigit2(argv[3]);
-    //cout << leap << endl;
-    const char* filename = argv[4];
-    //cout << filename << endl;;
+    int min_num;
+    int max_num;
+    int leap;
+    char filename[24];
+
+    cin >> filename >> min_num >> max_num >> leap;
 
     if( max_num == -1 || leap == -1 || min_num < 2 || max_num > 100){
         cout << "Enter proper values in the field. Now exiting.." << endl;
@@ -24,16 +22,41 @@ int main(int argc, char** argv){
     }
     unsigned int i = 0;
     unsigned int j = 0;
-    verbose__ = 0;
-    for(i = min_num; i < max_num; i += leap){
-        cout << "Total pageframes: " << i << endl;
+    for(i = min_num; i <= max_num; i += leap){
         main2(i, filename, opt);
-        main2(i, filename, fifo);
-        main2(i, filename, lru);
-        //cout << "misses_opt: " << misses_opt << endl;
-        //cout << "misses_lru: " << misses_fifo << endl;
-        //cout << "misses_lru: " << misses_lru << endl;
+        cout << "opt, " << i << " frames, Miss rate = " << misses_opt << "/" << total_access << " = " << (double)(misses_opt*100)/total_access << (char)37 << endl;
     }
+    cout << "------------------------------------------" << endl;
+    for(i = min_num; i <= max_num; i += leap){
+        main2(i, filename, fifo);
+        cout << "fifo, " << i << " frames, Miss rate = " << misses_fifo << "/" << total_access << " = " << (double)(misses_fifo*100)/total_access << (char)37 << endl;
+    }
+    cout << "-------------------------------------------" << endl;
+    for(i = min_num; i <= max_num; i += leap){
+        main2(i, filename, lru);
+        cout << "lru, " << i << " frames, Miss rate = " << misses_lru << "/" << total_access << " = " << (double)(misses_lru*100)/total_access << (char)37 << endl;
+    }
+
+    freopen("vmrates.dat","w",stdout);
+    for(i = min_num; i <= max_num; i += leap){
+        cout << i << " ";
+    }
+    cout << "\b" << endl;
+    for(i = min_num; i <= max_num; i += leap){
+        main2(i, filename, opt);
+        cout << (double)(misses_opt*100)/total_access << " ";
+    }
+    cout << "\b" << endl;
+    for(i = min_num; i <= max_num; i += leap){
+        main2(i, filename, fifo);
+        cout << (double)(misses_fifo*100)/total_access << " ";
+    }
+    cout << "\b" << endl;
+    for(i = min_num; i <= max_num; i += leap){
+        main2(i, filename, lru);
+        cout << (double)(misses_lru*100)/total_access << " ";
+    }
+    cout << "\b";
 
     return 0;
 }
